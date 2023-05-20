a6f856b08074ec3f8d6ed0e10b4b44084468c306
chaban-dmytro <dimach@ukr.net>
2023-05-20 11:27:29 +0200

    task-5

---
+++ js/task-5.js
@@ -0,0 +1,20 @@
+
+// Напишіть функцію для перевірки рядку на анаграму
+
+const isAnagram = function ( str1, str2 ) {
+  const normStr1 = str1.toLowerCase();
+  const normStr2 = str2.toLowerCase();
+
+  if ( normStr1.length !== normStr2.length ) {
+    return false;
+  } else if ( normStr1 === normStr2 ) {
+    return false;
+  } else {
+    const sortStr1 = normStr1.split( '' ).sort().join();
+    const sortStr2 = normStr2.split( '' ).sort().join();
+    return sortStr1 === sortStr2;
+  }
+}
+
+console.log( isAnagram( 'Abra', 'rBaaa'));
+
